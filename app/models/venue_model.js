0c8beea94f013e097dcb30ef7d080dd909c4c3af
Vlatko Trajkov <trajkovvlatko@gmail.com>
2019-10-27 19:58:16 +0100

    Get venue with all joins

--- app/models/venue_model.js
+++ app/models/venue_model.js
@@ -3,7 +3,8 @@ const db = require('../../config/database');
 class VenueModel {
   static async all(sorting, limit) {
     try {
-      const sql = [`
+      const sql = [
+        `
         SELECT venues.id, name, rating, 'venue' AS type, images.image
         FROM public.venues
         JOIN public.images
@@ -11,7 +12,8 @@ class VenueModel {
           AND images.owner_type = 'Venue'
           AND images.selected IS TRUE
         WHERE active IS TRUE
-      `];
+      `,
+      ];
       if (sorting === 'latest') {
         sql.push('ORDER BY id DESC');
       }
@@ -27,7 +29,74 @@ class VenueModel {
   static async find(id) {
     try {
       return await db.one(
-        'SELECT * FROM public.venues WHERE active IS TRUE AND id = $1',
+        `
+        SELECT
+          venues.*,
+          COALESCE(prop.list, '[]'::json) AS properties_list,
+          COALESCE(yt.list, '[]'::json) AS youtube_links_list,
+          COALESCE(img.list, '[]'::json) AS images_list,
+          COALESCE(booking.list, '[]'::json) AS bookings_list
+        FROM public.venues
+
+        JOIN LATERAL (
+          SELECT json_agg(
+            json_build_object(
+              'image'::text, images.image,
+              'selected'::text, images.selected
+            )
+          ) AS list
+          FROM public.images
+          WHERE images.owner_type = 'Venue'
+          AND images.owner_id = $1
+        ) img ON true
+
+        JOIN LATERAL (
+          SELECT json_agg(
+            json_build_object(
+              'link'::text, youtube_links.link
+            )
+          ) AS list
+          FROM public.youtube_links
+          WHERE youtube_links.owner_type = 'Venue'
+          AND youtube_links.owner_id = $1
+        ) yt ON true
+
+        JOIN LATERAL (
+          SELECT json_agg(
+            json_build_object(
+              'id'::text, properties.id,
+              'name'::text, properties.name
+            )
+          ) AS list
+          FROM public.properties
+
+          JOIN public.properties_venues
+            ON properties_venues.venue_id = venues.id
+            AND properties_venues.property_id = properties.id
+
+          WHERE properties.id = properties_venues.property_id
+          AND properties.active IS TRUE
+        ) prop ON true
+
+        JOIN LATERAL (
+          SELECT json_agg(
+            json_build_object(
+              'date'::text, bookings.booking_date,
+              'performer_id'::text, performers.id,
+              'performer_name'::text, performers.name
+            )
+          ) AS list
+          FROM public.bookings
+
+          JOIN public.performers
+            ON bookings.performer_id = performers.id
+
+          WHERE bookings.venue_id = $1
+        ) booking ON true
+
+        WHERE active IS TRUE
+        AND id = $1
+        `,
         id,
       );
     } catch (e) {
@@ -80,7 +149,6 @@ class VenueModel {
         data,
       );
     } catch (e) {
-      console.log(e)
       return {error: e};
     }
   }
