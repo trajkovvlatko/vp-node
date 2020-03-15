02bf5f68ee5fce845a8469a3419f2fd84efd851c
Vlatko Trajkov <trajkovvlatko@gmail.com>
2020-03-15 14:43:30 +0100

    Fixed seeds

--- config/seed.js
+++ config/seed.js
@@ -2,6 +2,10 @@ const UserModel = require('../app/models/user_model');
 const GenreModel = require('../app/models/genre_model');
 const PropertyModel = require('../app/models/property_model');
 const PerformerModel = require('../app/models/performer_model');
+const VenueModel = require('../app/models/venue_model');
+const YoutubeLinkModel = require('../app/models/youtube_link_model');
+const BookingModel = require('../app/models/booking_model');
+const ImageModel = require('../app/models/image_model');
 const db = require('./database');
 
 const locations = ['Sweden', 'Denmark', 'Norway', 'Finland'];
@@ -10,8 +14,8 @@ function randomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-async function addPerformer(user, i) {
-  const performer = await user.performers().create({
+async function addPerformer(userId, i) {
+  const performer = await PerformerModel.createForUser(userId, {
     name: `Performer ${i}`,
     location: randomElement(locations),
     phone: `${i}${i}${i}${i}${i}`,
@@ -24,14 +28,14 @@ async function addPerformer(user, i) {
     active: true,
   });
 
-  await user.images().create({
+  await ImageModel.createForUser(userId, {
     owner_id: performer.id,
     owner_type: 'Performer',
     image: `performers/${performer.id}/selected-image-${i}.jpg`,
     selected: true,
   });
 
-  await user.images().create({
+  await ImageModel.createForUser(userId, {
     owner_id: performer.id,
     owner_type: 'Performer',
     image: `performers/${performer.id}/image-${i}.jpg`,
@@ -48,21 +52,23 @@ async function addPerformer(user, i) {
     (genre_id, performer_id, created_at, updated_at)
     VALUES (${randomElement(genres).id}, ${performer.id}, now(), now())`);
 
-  await user.youtubeLinks().create({
-    owner_id: performer.id,
-    owner_type: 'Performer',
-    link: `http://performer-${performer.id}-youtube-1.asdf`,
-  });
-  await user.youtubeLinks().create({
-    owner_id: performer.id,
-    owner_type: 'Performer',
-    link: `http://performer-${performer.id}-youtube-2.asdf`,
-  });
+  await YoutubeLinkModel.createForOwner(
+    performer.id,
+    'Performer',
+    `http://performer-${performer.id}-youtube-1.asdf`,
+    userId,
+  );
+  await YoutubeLinkModel.createForOwner(
+    performer.id,
+    'Performer',
+    `http://performer-${performer.id}-youtube-2.asdf`,
+    userId,
+  );
   return performer;
 }
 
-async function addVenue(user, i) {
-  const venue = await user.venues().create({
+async function addVenue(userId, i) {
+  const venue = await VenueModel.createForUser(userId, {
     name: `Venue ${i}`,
     location: randomElement(locations),
     phone: `${i}${i}${i}${i}${i}`,
@@ -75,14 +81,14 @@ async function addVenue(user, i) {
     active: true,
   });
 
-  await user.images().create({
+  await ImageModel.createForUser(userId, {
     owner_id: venue.id,
     owner_type: 'Venue',
     image: `venues/${venue.id}/selected-image-${i}.jpg`,
     selected: true,
   });
 
-  await user.images().create({
+  await ImageModel.createForUser(userId, {
     owner_id: venue.id,
     owner_type: 'Venue',
     image: `venues/${venue.id}/image-${i}.jpg`,
@@ -99,21 +105,23 @@ async function addVenue(user, i) {
     (property_id, venue_id, created_at, updated_at)
     VALUES (${randomElement(properties).id}, ${venue.id}, now(), now())`);
 
-  await user.youtubeLinks().create({
-    owner_id: venue.id,
-    owner_type: 'Venue',
-    link: `http://venue-${venue.id}-youtube-1.asdf`,
-  });
-  await user.youtubeLinks().create({
-    owner_id: venue.id,
-    owner_type: 'Venue',
-    link: `http://venue-${venue.id}-youtube-2.asdf`,
-  });
+  await YoutubeLinkModel.createForOwner(
+    venue.id,
+    'Venue',
+    `http://venue-${venue.id}-youtube-1.asdf`,
+    userId,
+  );
+  await YoutubeLinkModel.createForOwner(
+    venue.id,
+    'Venue',
+    `http://venue-${venue.id}-youtube-2.asdf`,
+    userId,
+  );
   return venue;
 }
 
-async function addBooking(user, performer, venue) {
-  const booking = await user.bookings().create({
+async function addBooking(userId, performer, venue) {
+  const booking = await BookingModel.createForUser(userId, {
     performer_id: performer.id,
     venue_id: venue.id,
     booking_date: randomElement(['2012-01-01', '2012-01-02', '2012-01-03']),
@@ -160,9 +168,10 @@ async function addBooking(user, performer, venue) {
   }
 
   let performer, venue;
+
   for (let i = 1; i <= 10; i++) {
-    performer = await addPerformer(user, i);
-    venue = await addVenue(user, i);
+    performer = await addPerformer(user.data.id, i);
+    venue = await addVenue(user.data.id, i);
     await addBooking(user, performer, venue);
   }
 })();
